8a32db7e3556c50609d8ad0fa1cb963273e9571d
AJ Ibraheem <gai10@uni.brighton.ac.uk>
2016-01-26 16:47:59 +0000

    More datasets added and parsers

--- Sources/Main/main.swift
+++ Sources/Main/main.swift
@@ -2,11 +2,12 @@ import Foundation
 
 typealias Accuracy = Double
 
-func testAthletesDataSet( training:String, test:String ) throws -> Accuracy {
+func test( training training:String, test:String, testName:String, parser:DataParser ) -> Accuracy {
+    print("\n")
+    print("Using the \(testName) data set");
     print("Training with \(training)")
     print("Classfying with \(test)")
 
-    let parser = AtheletesParser();
     let classifier = Classifier( filename:training, dataParser:parser )
 
     let testData:[(classification:String, vector:[Double], ignore:[String])] = parser.parseFile( test );
@@ -20,6 +21,11 @@ func testAthletesDataSet( training:String, test:String ) throws -> Accuracy {
 
 var accuracy = 0.0
 
-print("\n")
-accuracy = try! testAthletesDataSet( "Data/athletesTrainingSet.txt", test:"Data/athletesTestSet.txt" )
+accuracy = test( training:"Data/athletesTrainingSet.txt", test:"Data/athletesTestSet.txt", testName: "Athlete", parser: AtheletesParser() )
+print("Accuracy is \(accuracy * 100)%")
+
+accuracy = test( training:"Data/mpgTrainingSet.txt", test:"Data/mpgTestSet.txt", testName: "Mile Per Gallon", parser: MPGParser() )
+print("Accuracy is \(accuracy * 100)%")
+
+accuracy = test( training:"Data/irisTrainingSet.data.txt", test:"Data/irisTestSet.data.txt", testName: "Iris", parser: IrisParser() )
 print("Accuracy is \(accuracy * 100)%")
