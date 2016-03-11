e7110d407f8e470a26b95953324bbd3d89979e54
AJ Ibraheem <gai10@uni.brighton.ac.uk>
2016-03-11 13:49:44 +0000

    Testing completed, classifer failed due to overflow in probability values most likely..... Clustering classifer begun

---
+++ Machine Learning/Sources/Main/HierarchicalClassifier.swift
@@ -0,0 +1,13 @@
+//
+//Author: Ibraheem AJ
+//  Date: 11/03/2016
+// Email: ibraheemaj@icloud.com
+//
+
+/**
+* Hierarchical Classifer 
+* Uses structured data to group the data into clusters
+*/
+
+class HierarchicalClassifer {
+}
