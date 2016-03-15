0cd1dd0f282abcf363ea41a105758e870c289a94
Ibraheem AJ <ibraheemaj@icloud.com>
2016-03-15 13:39:28 +0000

    Cluster code in progress

--- Machine Learning/Package.swift
+++ Machine Learning/Package.swift
@@ -3,7 +3,8 @@ import PackageDescription
 let package = Package(
     name: "Guide2DataMining",
     dependencies: [
-//        .Package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", versions: "0.1.0" ..< Version.max)
-//        .Package(url: "https://github.com/jkandzi/Table.swift", majorVersion: 0)//Draw beautiful tables to your terminal.
+        .Package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", versions: "0.1.0" ..< Version.max)
+        .Package(url: "https://github.com/jkandzi/Table.swift", majorVersion: 0)//Draw beautiful tables to your terminal.
+        .Package(url: "https://github.com/davecom/SwiftPriorityQueue", versions: Version(1,0,1)..<Version(1,0,3))
     ]
 )
--- Machine Learning/Sources/Main/HierarchicalClassifier.swift
+++ Machine Learning/Sources/Main/HierarchicalClassifier.swift
@@ -4,10 +4,15 @@
 // Email: ibraheemaj@icloud.com
 //
 
+import SwiftPriorityQueue
+
 /**
 * Hierarchical Classifer 
-* Uses structured data to group the data into clusters
+* Groups structured data into clusters. 
+* It utilises a priority queue to group the clusters into a Hierarchial Cluster with the priority being the shortest distance to a cluster's 
+* nearest neighbour
+* - todo:
+*   [x] Create a `Cluster` struct to represent a cluster
 */
-
 class HierarchicalClassifer {
 }
