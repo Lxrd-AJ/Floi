fbb1950ed6d6ed6a550127de7f8bb8b007661558
Ibraheem AJ <ibraheemaj@icloud.com>
2016-03-16 15:56:06 +0000

    Cluster implementation still in progress

--- Machine Learning/Sources/Main/Data Structures/Cluster.swift
+++ Machine Learning/Sources/Main/Data Structures/Cluster.swift
@@ -1,6 +1,12 @@
 
 //http://airspeedvelocity.net/2015/07/22/a-persistent-tree-using-indirect-enums-in-swift/
 indirect enum Cluster {
-  let content: String
-  let distance: Double
+    case Empty 
+    case ClusterNode( Cluster, index:Int, content:String )
 }
+
+
+
+
+  // let content: String
+  // let distance: Double
