e4531629464ac278289438e80483cae5a263bb02
AJ Ibraheem <gai10@uni.brighton.ac.uk>
2016-04-02 01:48:56 +0100

    Hierarchial Classifier complete

--- Sources/Main/Data Structures/Cluster.swift
+++ Sources/Main/Data Structures/Cluster.swift
@@ -1,3 +1,4 @@
+import Foundation 
 
 //http://airspeedvelocity.net/2015/07/22/a-persistent-tree-using-indirect-enums-in-swift/
 indirect enum Cluster {
@@ -8,5 +9,86 @@ indirect enum Cluster {
     
     init( name:String, distance:[Double] ){
       self = .ClusterNode(left:.Empty,distance:distance,content:name,right:.Empty)
+    }
+    
+    init( name:String, distance:[Double], left:Cluster, right:Cluster ){
+        self = .ClusterNode(left:left,distance:distance,content:name,right:right)
+    } 
+    
+    func values() -> (name:String, distance:[Double]){
+        switch self {
+            case .Empty:
+                return ("",[])
+            case .ClusterNode(_,let dist,let cnt,_):
+                return (cnt,dist)
+        }
+    }
+    
+    /**
+    Returns the Euclidean distance between the current cluster to the specified cluster 
+    */
+    func distanceTo( cluster: Cluster ) -> Double {
+        switch (cluster,self) {
+        case (.Empty,_):
+            return 0.0
+        case (.ClusterNode(_,let vector1,_,_), .ClusterNode(_,let vector2,_,_) ):
+            let sumSqaures = Array(zip(vector1,vector2)).map({ ($0.0 - $0.1) ^^ 2 }).reduce(0, combine:+)
+            return sqrt( sumSqaures )
+        default:
+            return 0.0
+        }
+    }
+    
+    /**
+    The Nearest Neighbour using the Euclidean distance function 
+    */
+    func nearestNeighbour( clusters:[Cluster] ) -> Cluster {
+        //check if `self` in `clusters` and remove if present
+        guard clusters.count > 1 else { return clusters[0] }
+        let clusters = clusters.remove(self)
+        return clusters.sort({ return (self.distanceTo($0)) < (self.distanceTo($1)) }).first! //print("\($0)\n\($1)");
+    }
+    
+    func distanceToNearestNeighbour( clusters:[Cluster] ) -> Double {
+        let nearestNeighbour = self.nearestNeighbour(clusters)
+        return self.distanceTo( nearestNeighbour )
+    }
+}
+
+extension Cluster: Equatable {
+}
+func ==(lhs: Cluster, rhs:Cluster) -> Bool {
+    switch(lhs,rhs){
+        case (.ClusterNode(_,_,let c1,_),.ClusterNode(_,_,let c2,_)) where c1 == c2:
+            return true 
+        case (.Empty,.Empty):
+            return true 
+        default:
+            return false 
+    }
+}
+
+extension Array where Element: Equatable{
+    /**
+    Returns an array without the specified paramter if it was present
+    */
+    func remove( cluster:Element ) -> [Element]{
+        let idx = self.indexOf(cluster)
+        var result = Array(self)
+        if let index = idx {
+            result.removeAtIndex(index)
+        }
+        return result
+    }
+    
+    /**
+    Helper function to remove an array of elements
+    */
+    func remove( clusters:[Element] ) -> [Element]{
+        var result:[Element] = []
+        for i in 0..<clusters.count {
+            result = self.remove(clusters[i])
+        }
+        return result;
     }
 }
--- Sources/Main/HierarchicalClassifier.swift
+++ Sources/Main/HierarchicalClassifier.swift
@@ -52,7 +52,61 @@ class HierarchicalClassifier {
     
     init( filename:String ){
         self.clusters = ClusterParser.parseFile( filename ); 
-        print( self.normalizedClusters )
+        //print( self.normalizedClusters )
+        //print("Nearest Neighbour")
+        //print( self.normalizedClusters[0].nearestNeighbour(self.normalizedClusters)
+        
+        let sortedClusters = self.normalizedClusters.sort({ (cluster1,cluster2) in 
+            let rest = self.normalizedClusters
+            return cluster1.distanceToNearestNeighbour(rest) < cluster2.distanceToNearestNeighbour(rest)
+        })
+        
+        let dendogram = self.createTree( sortedClusters[0], clusters:Array(sortedClusters[1..<sortedClusters.count]) )
+        prettify(dendogram)
+        
+    }
+    
+    /**
+    - warning: Incomplete implementation 
+    */
+    func prettify( tree:Cluster ){
+        
+        switch tree {
+        case .Empty:
+            print("--")
+        case .ClusterNode(let lhs,_, let name,let rhs):
+            print("\t\(name)\n")
+            prettify(lhs)
+            prettify(rhs)
+        }
+        
+    }
+    /**
+    Sorts and clusters the elements recursively based on their nearest neighbour and returns a tree/dendogram of the
+    clusters 
+    */
+    func createTree( cluster:Cluster, clusters:[Cluster] ) -> Cluster {
+        if clusters.count == 0 { 
+            return cluster 
+        }else{
+            let lhs = cluster
+            let rhs = lhs.nearestNeighbour(clusters);
+            let name = lhs.values().name + "," + rhs.values().name 
+            let newDist = min(lhs,y:rhs,set:clusters).values().distance
+            let newCluster = Cluster(name:name,distance:newDist,left:lhs,right:rhs); 
+            //print(name)
+            return self.createTree( newCluster, clusters: clusters.remove(rhs) )
+        }
+    }
+    
+    /**
+    Returns the cluster with the whose closest nearest neighbour's distance is lesser than the other in the sample
+    space `set`
+    */
+    private func min( x:Cluster, y:Cluster, set:[Cluster] ) -> Cluster {
+        if x.distanceToNearestNeighbour(set) < y.distanceToNearestNeighbour(set){
+            return x
+        }else{ return y }
     }
     
     /**
